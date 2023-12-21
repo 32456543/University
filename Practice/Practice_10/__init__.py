dd011daeeff5a1cf56bb2d766831f8e785c79bae
holite <144428331+32456543@users.noreply.github.com>
2023-12-21 22:17:26 +0300

    a

--- Practice/Practice_10/__init__.py
+++
@@ -1,7 +0,0 @@
-try:
-    from .program import *
-    from .file_change import *
-    from .viewer import *
-except Exception:
-    print('Че-то не то')
-    input()
--- Practice/Practice_10/__main__.py
+++
@@ -1,4 +0,0 @@
-from program import *
-
-if __name__ == '__main__':
-    office()
